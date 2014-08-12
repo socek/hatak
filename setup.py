d981660467b454284cc295227a3a98f076f96443
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-08-12 22:14:11 +0200

    Fixed setup.py for namespace bael.

--- setup.py
+++ setup.py
@@ -9,23 +9,24 @@ install_requires = [
 
 
 if __name__ == '__main__':
-    setup(name='hatak',
-          version=VERSION,
-          license='Apache License 2.0',
-          packages=find_packages('src'),
-          package_dir={'': 'src'},
-          install_requires=install_requires,
-          namespace_packages=['bael'],
-          include_package_data=True,
-          zip_safe=False,
-          package_data={
-              '': [
-                  'hatak/templates/*.ini',
-                  'hatak/templates/*.tpl',
-                  'hatak/templates/project/*.tpl'],
-          },
-          entry_points="""\
-              [console_scripts]
-                  hatak = bael.hatak.recipe:run
-          """,
-          )
+    setup(
+        name='hatak',
+        version=VERSION,
+        description='Small Pyramid extension/project managment',
+        license='Apache License 2.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=install_requires,
+        namespace_packages=['bael'],
+        zip_safe=False,
+        package_data={
+            '': [
+                'hatak/templates/*.ini',
+                'hatak/templates/*.tpl',
+                'hatak/templates/project/*.tpl'],
+        },
+        entry_points="""\
+          [console_scripts]
+              hatak = bael.hatak.recipe:run
+        """,
+        )
