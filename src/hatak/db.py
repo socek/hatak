8fca05f26515209b149e743e3e39e299cff9c860
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-08-21 22:07:14 +0200

    Imroved db model.

--- src/hatak/db.py
+++ src/hatak/db.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.ext.declarative import AbstractConcreteBase
 
+from .unpackrequest import unpack
+
 DeclatativeBase = declarative_base()
 
 
@@ -26,10 +28,35 @@ class Base(AbstractConcreteBase, DeclatativeBase):
     def get_or_create(cls, *args, **kwargs):
         return get_or_create(cls, *args, **kwargs)
 
+    @classmethod
+    def _query(cls, db):
+        return db.query(cls)
+
+    @classmethod
+    def get_all(cls, db):
+        return cls._query(db).all()
+
+    @classmethod
+    def get_by_id(cls, db, id_):
+        return cls._query(db).filter_by(id=id_).one()
+
+    @classmethod
+    def create(cls, db, *args, **kwargs):
+        obj = cls(*args, **kwargs)
+        db.add(obj)
+        db.commit()
+        return obj
+
+    @classmethod
+    def delete_by_id(cls, db, id_):
+        obj = cls.get_by_id(db, id_)
+        db.delete(obj)
+        db.commit()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.request = None
 
     def assign_request(self, request):
         self.request = request
-        self.registry = request.registry
+        unpack(self, request)
