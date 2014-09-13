7977c116f84b2744f4acc373d83941603d126673
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-09-13 17:49:46 +0200

    Database connection is now creating per worker, but it is flushed on every request.

--- src/hatak/plugins/sql.py
+++ src/hatak/plugins/sql.py
@@ -9,11 +9,13 @@ class SqlPlugin(Plugin):
     def add_to_registry(self):
         engine = create_engine(self.settings['db:url'])
         self.config.registry['db_engine'] = engine
+        self.config.registry['db'] = sessionmaker(bind=engine)()
         self.config.add_request_method(self.add_db, 'db', reify=True)
 
     def add_db(self, request):
-        engine = request.registry['db_engine']
-        return sessionmaker(bind=engine)()
+        db = request.registry['db']
+        db.flush()
+        return db
 
     def add_unpackers(self, unpacker):
         unpacker.add('db', lambda req: req.db)
